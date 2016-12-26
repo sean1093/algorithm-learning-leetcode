2d1c2227c7d64009c0c8057d5064b7e6b5eff076
sean1093 <sean1093@gmail.com>
2016-12-26 11:38:25 +0800

    two sum

--- twoSum.js
+++ twoSum.js
@@ -7,16 +7,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-	for(var i = 0; i < nums.length; i++){  	
-		for(var j = i+1; j < nums.length; j++){
-			var sum = nums[i] + nums[j];
-			if(sum == target){
-				var ansArray = [];
-				ansArray.push(i);
-				ansArray.push(j);
-				return ansArray;
-			}  			
-		}
+    for(var i = 0; i < nums.length; i++){  	
+        for(var j = i+1; j < nums.length; j++){
+            var sum = nums[i] + nums[j];
+            if(sum == target){
+                var ansArray = [];
+                ansArray.push(i);
+                ansArray.push(j);
+                return ansArray;
+            }  			
+        }
     }
 };
 
