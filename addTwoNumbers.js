34cadb4db5f7b5b9ba9d8161e692595b458d7d12
sean1093 <sean1093@gmail.com>
2016-12-26 13:56:02 +0800

    add two number

---
+++ addTwoNumbers.js
@@ -0,0 +1,19 @@
+/**
+ * You are given two linked lists representing two non-negative numbers. 
+ * The digits are stored in reverse order and each of their nodes contain a single digit. 
+ * Add the two numbers and return it as a linked list.
+ * 
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    
+};
