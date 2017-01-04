820f229bec8db84119a30e9df10707d3bc349560
sean1093 <sean1093@gmail.com>
2017-01-04 20:31:54 +0800

    atoi

--- algorithms/string-tointeger-atoi.js
+++ algorithms/string-tointeger-atoi.js
@@ -1,77 +1,78 @@
 /**
- * 08. String to Integer (atoi) - dev
+ * 08. String to Integer (atoi)
  * Implement atoi to convert a string to an integer.
  * @param {string} str
  * @return {number}
  */
 var myAtoi = function(str) {
+    var INT_MAX = Math.pow(2,31)-1; //overflows
     var l = str.length;
-    var flag = true, start = true, n = 0;
+    var flag = null, start = true, n = 0;
     var strArray = str.split("");
-    var count = l;
     for(var i in strArray) {
-    	// console.log(strArray[i]); 
-    	console.log("n= "+n); 
-    	console.log("i, l= "+i+", "+l); 
-    	// console.log("n=0? "+n==0);
-    	if(strArray[i] == " "){
-    		if(start){
-    			l--;
-    		}
-    		else{
-    			n = n / Math.pow(10, (i-l));
-	            return flag? n : n*-1;
-    		}
-    	}
-		else if(strArray[i] == "-" || strArray[i] == "+") {
-			flag = (strArray[i] == "-")? false : true; //negative
-			l--; 
-			start = false;
-		}
-		else{
-	        var base = strArray[i] * Math.pow(10, (i-l));
-	        console.log("base= "+base); 
-	        if(isNaN(base)){
-	            n = n / Math.pow(10, (i-l));
-	            return flag? n : n*-1;
-	        }
-	        n += base;
-	        start = false;
-	        l--;
-		}
+        if(strArray[i] != " " || start === false){
+            start = false;
+            if((strArray[i] == "-" || strArray[i] == "+") && flag === null) {
+                flag = (strArray[i] == "-")? false : true; //negative
+            }
+            else{
+                var base = strArray[i] * Math.pow(10, (l-1));
+                if(isNaN(base) || strArray[i] == " "){
+                    n = n / Math.pow(10, (l));
+                    break;
+                }
+                n += base;
+            }
+        }
+        l--;          
     }
-    return flag? n : n*-1;
+    n = (flag === true || flag === null)? n : n*-1;
+    if(n > INT_MAX) n = INT_MAX;
+    if(n < -(1+INT_MAX) ) n = -(1+INT_MAX);
+    return n;
 };
 
-// console.log("123"); 
-// console.log(myAtoi("123")); 
+console.log("123"); 
+console.log(myAtoi("123")); 
 
-// console.log("0"); 
-// console.log(myAtoi("0")); 
+console.log("0"); 
+console.log(myAtoi("0")); 
 
-// console.log(""); 
-// console.log(myAtoi("")); //0
+console.log(""); 
+console.log(myAtoi("")); //0
 
-// console.log("-36"); 
-// console.log(myAtoi("-36")); 
+console.log("-36"); 
+console.log(myAtoi("-36")); 
 
-// console.log("aaa"); 
-// console.log(myAtoi("aaa")); //0
+console.log("aaa"); 
+console.log(myAtoi("aaa")); //0
 
-// console.log("66.42"); 
-// console.log(myAtoi("66.42")); //66 
+console.log("66.42"); 
+console.log(myAtoi("66.42")); //66 
 
-// console.log("-36.66"); 
-// console.log(myAtoi("-36.66")); // -36
+console.log("-36.66"); 
+console.log(myAtoi("-36.66")); // -36
 
-// console.log("+1"); 
-// console.log(myAtoi("+1")); //1
+console.log("+1"); 
+console.log(myAtoi("+1")); //1
 
 console.log("     +004500"); 
 console.log(myAtoi("     +004500")); //4500 
 
-// console.log("   +0 123"); 
-// console.log(myAtoi("   +0 123")); //0
+console.log("   +0 123"); 
+console.log(myAtoi("   +0 123")); //0
 
-// console.log("1 23"); 
-// console.log(myAtoi("1 23")); //1
+console.log("1 23"); 
+console.log(myAtoi("1 23")); //1
+
+console.log("+-2"); 
+console.log(myAtoi("+-2")); //0
+
+console.log("2147483648"); 
+console.log(myAtoi("2147483648")); //2147483647
+
+console.log("-214748369948"); 
+console.log(myAtoi("-214748369948")); //-2147483648
+
+console.log("1a"); 
+console.log(myAtoi("1a")); //1
