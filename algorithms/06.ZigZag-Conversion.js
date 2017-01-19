bb4869488a12bf00ce3b83edb2c43d0dbb9ba121
sean1093 <sean1093@gmail.com>
2017-01-19 20:44:42 +0800

    ZigZag Conversion

--- algorithms/06.ZigZag-Conversion.js
+++ algorithms/06.ZigZag-Conversion.js
@@ -7,6 +7,8 @@
  */
 var convert = function(s, numRows) {
 
+    if(numRows <= 1 ) return s;
+
     var locate = [];
     for(var idx = 0; idx < numRows; idx++) {
         locate[idx] = [];
@@ -16,12 +18,30 @@ var convert = function(s, numRows) {
     var r = 0;
     var c = 0;
     var flag = 0;
-    for(var i = 0; i < str.length; i++) {
-        locate[r][c] = str[i]; 
+    for(var k = 0; k < str.length; k++) {
+        locate[r][c] = str[k]; 
+        if(r === 0) flag = true;
+        if(r == (numRows-1)) flag = false;
+        if(flag) r++;
+        else {
+            r--;
+            c++;
+        } 
     }
-    
+    var re = "";
+    for(var i = 0; i < numRows; i++) {
+        for(var j = 0; j <= c; j++) {
+            if(locate[i][j] !== undefined)
+                re += locate[i][j];
+        }
+    }
+
+    return re;   
 };
 
+console.log("ABCDE");
+console.log(convert("ABCDE", 3)); //"AEBDC"
 
 console.log("PAYPALISHIRING");
 console.log(convert("PAYPALISHIRING", 3)); //"PAHNAPLSIIGYIR"
+
