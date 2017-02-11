33300508b0c8db19a075b62807affd5c3783767e
sean1093 <sean1093@gmail.com>
2017-02-11 15:39:11 +0800

    475. Heaters

--- algorithms/475.Heaters.js
+++ algorithms/475.Heaters.js
@@ -1,6 +1,6 @@
 /**
  * 475. Heaters
- * **** not finsish
+ * find max(min())
  * @param {number[]} houses
  * @param {number[]} heaters
  * @return {number}
@@ -12,32 +12,22 @@ var findRadius = function(houses, heaters) {
     }
     heaters.sort(sortNumber);
     houses.sort(sortNumber);
-
-    console.log(houses);
-    console.log(heaters);
-
-    var radius = heaters[0] - 1; //first
-    var r = 0;
-    var i = 1;  
-    // console.log("heaters.length: "+heaters.length);  
-    for(; i < heaters.length; i++) {
-        // console.log("heaters[i]: "+heaters[i]);
-        // console.log("houses[houses.length-1]: "+houses[houses.length-1]);
-        if(heaters[i] > houses[houses.length-1]) {
-            break;
+    var h = houses.length;
+    var t = heaters.length;
+    var radius = 0;
+    for(var i=0; i<h; i++){
+        var min = houses[h-1] > heaters[t-1]? houses[h-1]:heaters[t-1];
+        //find min
+        for(var j = 0; j<t ; j++){
+            var diff = Math.abs(heaters[j]-houses[i]);
+            min = min >= diff? diff : min;
+            if(diff > min) break;
         }
-        r = Math.ceil((heaters[i] - heaters[i-1] - 1)/2);
-        radius = r > radius? r : radius; 
+        //find max
+        radius = radius < min? min : radius;
     }
-    // console.log("i: "+i);
-    //last
-    r = houses[houses.length-1] - heaters[i-1];
-    radius = r > radius? r : radius;
-
     return radius;  
 };
-
-
 console.log(findRadius([1,2,3],[2])); // 1
 console.log(findRadius([1,2,3,4],[1,4])); // 1
 console.log(findRadius([1,2,3,5,8],[3,7])); // 2
@@ -50,4 +40,6 @@ console.log(findRadius([282475249,622650073,984943658,144108930,
     470211272,101027544,457850878,458777923],
 [823564440,115438165,784484492,74243042,
 114807987,137522503,441282327,16531729,823378840,143542612])); //161834419 
+
+console.log(findRadius([1,5],[10])); // 9
 
