e92ebc505dac76a9f57da50e35d6969329583034
sean1093 <sean1093@gmail.com>
2017-02-13 20:54:56 +0800

    504. Base 7

--- algorithms/504.Base-7.js
+++ algorithms/504.Base-7.js
@@ -5,57 +5,29 @@
  * @return {string}
  */
 var convertTo7 = function(num) {
-    var str = "";
-    var base = num;
+    var str = "";    
     var left = 0;
-    while(base>7){
-
-        base = Math.floor(base/7);
-        console.log("base: "+base); 
-        // console.log("(base*7): "+(base*7));
-        // console.log("num: "+num);
-        // num = num - (base*7);
-        left = Math.floor(num % 7);
-        str += left;
-        
-        
-        console.log("left: "+left);
-
-        console.log("---str: "+str);
+    var flag = false;
+    if(num===0) {
+        return 0;
     }
-    console.log("num: "+num.toString(7));
+    else if(num < 0) {
+        flag = true;
+        num = Math.abs(num);
+    }
+    var base = num;
+    while(base>7) {
+        base = Math.floor(num / 7);      
+        str += Math.floor(num % 7);
+        num = base;
+    }
+    str += base;
+    if(flag) {
+        str = "-" + str;
+    }
     return str;
 };
 
-/*
-class Solution {
-public:
-    string convertTo7(int num) {
-        if(num == 0)
-            return "0";
-        string ans = "";
-        bool neg = false;
-        if(num < 0)
-        {
-            neg = true;
-            num = -num;
-        }
-        while(num)
-        {
-            string tmp = ans;
-            ans = "";
-            ans += (char) ('0' + (num % 7));
-            ans += tmp;
-            num /= 7;
-        }
-        if(neg)
-            ans = "-" + ans;
-        return ans;
-    }
-};
-
-
-
-*/
-
 console.log(convertTo7(100)); // "202"
+console.log(convertTo7(-100)); // "-202"
+console.log(convertTo7(-7)); // "-10"
