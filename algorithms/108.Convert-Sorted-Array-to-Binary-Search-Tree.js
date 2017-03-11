a438d1125da6af4473805e73f12c57bb75daa466
sean1093 <sean1093@gmail.com>
2017-03-11 16:52:44 +0800

    108. Convert Sorted Array to Binary Search Tree

--- algorithms/108.Convert-Sorted-Array-to-Binary-Search-Tree.js
+++ algorithms/108.Convert-Sorted-Array-to-Binary-Search-Tree.js
@@ -1,24 +1,21 @@
 /**
  * 108. Convert Sorted Array to Binary Search Tree
- * dev
  * @param {number[]} nums
  * @return {TreeNode}
  */
 //Divide and Conquer 
 var sortedArrayToBST = function(nums) {
-    function setTree(array){
-        var mid = Math.round((array.length-1)/2);
-        // console.log("array[mid]:"+array[mid]);
+    function setTree(array, start, end){
+        if(start > end){
+            return null;
+        }
+        var mid = start + parseInt((end - start)/2);
         var node = new TreeNode(array[mid]);
-        var left = array.slice(0, mid);
-        var right = array.slice(mid+1, array.length);
-        // console.log("left:"+left);
-        // console.log("right:"+right);
-        node.left = left.length !== 0? setTree(left): null;
-        node.right = right.length !== 0? setTree(right): null;
+        node.left = setTree(array, start, mid-1);
+        node.right = setTree(array, mid+1, end);        
         return node;
     }
-    return setTree(nums);
+    return setTree(nums, 0, nums.length-1);
 };
 
 // Definition for a binary tree node.
@@ -26,6 +23,5 @@ function TreeNode(val) {
     this.val = val;
     this.left = this.right = null;
 }
-// console.log(sortedArrayToBST([0,1,2,3,4,5,6,7,8,9,10])); //[2,1,3]
-
+console.log(sortedArrayToBST([0,1,2,3,4,5,6,7,8,9,10])); //[5,2,8,0,3,6,9,null,1,null,4,null,7,null,10]
 console.log(sortedArrayToBST([1,2,3])); //[2,1,3]
