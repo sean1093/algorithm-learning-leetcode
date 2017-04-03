39fd8ee1c43f3dd2ca6f86643bb3d8da6ca8a9f3
sean1093 <sean1093@gmail.com>
2017-04-03 16:02:53 +0800

    118. Pascals Triangle

---
+++ algorithms/118.Pascal's-Triangle.js
@@ -0,0 +1,26 @@
+/**
+ * 118. Pascal's Triangle
+ * Given numRows, generate the first numRows of Pascal's triangle.
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+ // [i,j] = [i-1,j-1] + [i-1, j]
+var generate = function(numRows) {   
+    if(numRows === 0) return [];
+    var res = [[1]];
+    for(var i = 1; i < numRows; i++) {
+        var preRow = res[i-1];
+        var row = [1];
+        for(var j = 1; j <= i; j++) {
+            var pre =  preRow[j-1];
+            var cur =  preRow[j] ? preRow[j] : 0;
+            row.push(pre+cur);  
+        }
+        res.push(row);
+    }
+    return res;
+};
+
+console.log(generate(0)); //[]
+console.log(generate(1)); //[[1]]
+console.log(generate(5)); //[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
