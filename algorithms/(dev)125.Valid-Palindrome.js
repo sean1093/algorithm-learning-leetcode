e055cc2aa56560ec3ea439c84f34cfd24044b165
sean1093 <sean1093@gmail.com>
2017-05-07 22:06:33 +0800

    update

---
+++ algorithms/(dev)125.Valid-Palindrome.js
@@ -0,0 +1,12 @@
+/**
+ * 125. Valid Palindrome
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    
+};
+
+console.log(isPalindrome()); // true
+console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
+console.log(isPalindrome("race a car")); // false
