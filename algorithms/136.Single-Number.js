a16f388646782f946b6a6610cd975ca5e23c8d25
sean1093 <sean1093@gmail.com>
2017-08-02 20:11:26 +0800

    136. Single Number

--- algorithms/136.Single-Number.js
+++ algorithms/136.Single-Number.js
@@ -4,7 +4,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-// concept: Bitwise Operators XOR
+
+// Q: 一個陣列中，所有數字都會出現兩次，只有一個只出現一次，找出這一個
+// A: concept: Bitwise Operators XOR -> a ^ b (XOR): 其中一個是1才會回傳1，兩個都是1的話會回傳0
+// 利用XOR，因為自己跟自己XOR會是0，任何數跟0做XOR或是自己
+// ex: [1,1,2]
+// 1 ^ 1 = 0 (1 ^ 1 = 0)
+// 0 ^ 2 = 2 (00 ^ 11 = 11)
+
 var singleNumber = function(nums) {
     var r = 0;
     for (var key in nums) {
