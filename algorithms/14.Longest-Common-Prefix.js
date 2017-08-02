73315f1c4b3c02d2e9642c4c13e5671df70199b3
sean1093 <sean1093@gmail.com>
2017-08-02 21:56:07 +0800

    14. Longest Common Prefix

--- algorithms/14.Longest-Common-Prefix.js
+++ algorithms/14.Longest-Common-Prefix.js
@@ -4,6 +4,11 @@
  * @param {string[]} strs
  * @return {string}
  */
+// Q: 給一個 string[]，找出這些字串中共同最長的prefix
+// A: 利用刪去法
+// 1. 先把第一個字串當作 prefix 
+// 2. 開始從第二個檢查，從第一個字母往後，發現有跟 prefix 不一樣的字的時候就 break 並且裁切 prefix 到不一樣之前
+
 var longestCommonPrefix = function(strs) {
     var prefix = "";
     if(strs.length === 0 || strs === null) return prefix;
